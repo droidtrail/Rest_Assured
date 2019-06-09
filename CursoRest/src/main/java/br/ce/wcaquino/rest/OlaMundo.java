e7ae652e0bc37d197c7cbac1eee3be1f90a9ede5
Leandro Pereira <leandro.nares@gmail.com>
2019-06-08 21:33:43 -0300

    Ola, Mundo.

--- CursoRest/src/main/java/br/ce/wcaquino/rest/OlaMundo.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/OlaMundo.java
@@ -9,6 +9,7 @@ public class OlaMundo {
 	public static void main(String[] args) {
 		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
 		System.out.println(response.getBody().asString());
+		// System.out.println(response.statusCode());
 
 	}
 
