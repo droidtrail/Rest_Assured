4a0ab4bd6ea184c46d520297a785f6ae9767049c
Leandro Pereira <leandro.nares@gmail.com>
2019-06-08 23:01:37 -0300

    Modo Fluente.

--- CursoRest/src/main/java/br/ce/wcaquino/rest/OlaMundoTeste.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/OlaMundoTeste.java
@@ -1,6 +1,11 @@
 package br.ce.wcaquino.rest;
 
-import org.junit.Assert;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import io.restassured.RestAssured;
@@ -12,14 +17,31 @@ public class OlaMundoTeste {
 
 	@Test
 	public void testOlaMundo() {
-		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
-		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
-		Assert.assertTrue(response.statusCode() == 200);
-		Assert.assertTrue("O status code deveria ser 200.",response.statusCode() == 200);
-		Assert.assertEquals(200,response.statusCode());//Primeiro parâmetro: Esperado - Segundo parâmetro: o que veio do site no método GET
+		Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
+		assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		assertTrue(response.statusCode() == 200);
+		assertTrue("O status code deveria ser 200.", response.statusCode() == 200);
+		assertEquals(200, response.statusCode());// Primeiro parâmetro: Esperado - Segundo parâmetro: o que veio
+														// do site no método GET
 
 		ValidatableResponse validacao = response.then();
 		validacao.statusCode(200);
+	}
+
+	@Test
+	public void devoConhecerOutrasFormasRestAssured() {
+		Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+		
+		get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+		
+		given()//Pré condições
+		.when() //Ação
+			.get("http://restapi.wcaquino.me/ola")
+		.then()//Assertivas
+			.statusCode(200);
+
 	}
 
 }
