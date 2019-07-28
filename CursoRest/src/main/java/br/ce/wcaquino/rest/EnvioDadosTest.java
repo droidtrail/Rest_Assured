dbae32ef3e7b6b0c5997270878807dff7fa0b58d
Leandro Pereira <leandro.nares@gmail.com>
2019-07-27 22:55:39 -0300

    Query parametrizável

--- CursoRest/src/main/java/br/ce/wcaquino/rest/EnvioDadosTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/EnvioDadosTest.java
@@ -1,7 +1,9 @@
 package br.ce.wcaquino.rest;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import io.restassured.http.ContentType;
@@ -19,6 +21,24 @@ public class EnvioDadosTest {
 			.log().all()
 			.statusCode(200)
 			.contentType(ContentType.JSON)
+		
+		;	
+	}
+	
+	@Test
+	public void deveEnviarValorViaQueryViaParamtro() {
+		
+		given()
+			.log().all()
+			.queryParam("format", "xml")
+			.queryParam("outra", "coisa")
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML)
+			.contentType(containsString("utf-8"))
 		
 		;	
 	}
