1c2fde8f9461f878ccb379a6cd82a15ff724729c
Leandro Pereira <leandro.nares@gmail.com>
2019-08-17 09:11:16 -0300

    Validando Json.

--- CursoRest/src/main/java/br/ce/wcaquino/rest/SchemaTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/SchemaTest.java
@@ -5,6 +5,8 @@ import static io.restassured.RestAssured.given;
 import org.junit.Test;
 
 import io.restassured.matcher.RestAssuredMatchers;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
 import org.xml.sax.SAXParseException;
 
 public class SchemaTest {
@@ -37,8 +39,20 @@ public class SchemaTest {
 			.statusCode(200)
 			.body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
 		;
-		
-		
+			
 	}
-
+	
+	@Test
+	public void deveValidarSchemaJson(){
+		
+		given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("users.json"))
+		;	
+	}
 }
