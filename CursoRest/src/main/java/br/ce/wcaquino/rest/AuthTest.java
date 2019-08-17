8e81c6061f8cd9ab8b7fe410c5897e8ffc30903c
Leandro Pereira <leandro.nares@gmail.com>
2019-08-17 14:02:28 -0300

    Acessando API com chave

--- CursoRest/src/main/java/br/ce/wcaquino/rest/AuthTest.java
+++ CursoRest/src/main/java/br/ce/wcaquino/rest/AuthTest.java
@@ -1,7 +1,7 @@
 package br.ce.wcaquino.rest;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
@@ -27,6 +27,30 @@ public class AuthTest {
 			.body("gender", is("male"))
 			.body("homeworld", is("https://swapi.co/api/planets/1/"))
 		;
+		//395c91b2a74d671af477b24a59fab403
+		//http://api.openweathermap.org/data/2.5/weather?q=Rio%20de%20Janeiro&appid=395c91b2a74d671af477b24a59fab403&&units=metric
+		
+	}
+	
+	@Test
+	public void deveObterClima() {
+		
+		given()
+		.log().all()
+		.queryParam("q","Rio de Janeiro")
+		.queryParam("appid","395c91b2a74d671af477b24a59fab403")
+		.queryParam("units","metric")
+	.when()
+		.get("http://api.openweathermap.org/data/2.5/weather")
+	.then()
+		.log().all()
+		.statusCode(200)
+		.body("name", is("Rio de Janeiro"))
+		.body("coord.lon", is(-43.21f))
+		.body("main.temp", greaterThan(25f))
+		
+		
+	;
 		
 	}
 
